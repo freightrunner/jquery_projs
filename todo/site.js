3f80fde4650b3c828f5ce59885ddaa0d5eff521a
Brian Arpaio <arpaio@gmail.com>
2017-06-20 15:10:08 +0000

    working on simple jQuery todo list

---
+++ todo/site.js
@@ -0,0 +1,9 @@
+$("#add").click(function() {
+  var val = $("input").val();
+  if(val !== "") {
+    var elem = $("<li></li>").text(val);
+    $(elem).append("<button class='rem'>X</button>");
+    $("#mylist").append(elem);
+    $("input").val("");
+  }
+});
